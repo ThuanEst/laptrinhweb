f4d85d086f8fcc1151302a0b18b0d17357420b3a
ThuanEst <vanthuan021102@gmail.com>
2024-04-02 20:38:11 +0700

    create a database folder contain 3 files

---
+++ database/bkc.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1:3307
+-- Thời gian đã tạo: Th4 02, 2024 lúc 02:58 PM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `bkc`
+--
+CREATE DATABASE IF NOT EXISTS `bkc` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `bkc`;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `order_details`
+--
+
+DROP TABLE IF EXISTS `order_details`;
+CREATE TABLE `order_details` (
+  `order_detail_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_price` double NOT NULL,
+  `product_description` text NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(25) NOT NULL,
+  `user_email` varchar(55) NOT NULL,
+  `user_pass` int(255) NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `_user_id` (`user_id`);
+
+--
+-- Chỉ mục cho bảng `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`order_detail_id`),
+  ADD KEY `_order_id` (`order_id`),
+  ADD KEY `_product_id` (`product_id`);
+
+--
+-- Chỉ mục cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `order_detail_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ database/solution_b.sql
@@ -0,0 +1,169 @@
+﻿--- 1. Liệt kê các hóa đơn của khách hàng, thông tin hiển thị gồm: mã user, tên user, mã hóa đơn
+	SELECT users.user_id, users.user_name, orders.order_id			
+	FROM users			
+	INNER JOIN orders ON users.user_id = orders.user_id;			
+--- 2. Liệt kê số lượng các hóa đơn của khách hàng: mã user, tên user, số đơn hàng
+	SELECT u.user_id, u.user_name, COUNT(o.order_id) AS so_don_hang
+	FROM users u
+	LEFT JOIN orders o ON u.user_id = o.user_id
+	GROUP BY u.user_id, u.user_name;
+--- 3. Liệt kê thông tin hóa đơn: mã đơn hàng, số sản phẩm
+	SELECT order_id, COUNT(product_id) AS so_san_pham
+	FROM order_details
+	GROUP BY order_id;
+--- 4. Liệt kê thông tin mua hàng của người dùng: mã user, tên user, mã đơn hàng, tên sản phẩm. Lưu ý: gôm nhóm theo đơn hàng, tránh hiển thị xen kẻ các đơn hàng với nhau
+	SELECT 
+		u.user_id,
+		u.user_name,
+		od.order_id,
+		p.product_name
+	FROM 
+		users u
+	INNER JOIN 
+		orders o ON u.user_id = o.user_id
+	INNER JOIN 
+		order_details od ON o.order_id = od.order_id
+	INNER JOIN 
+		products p ON od.product_id = p.product_id
+	ORDER BY 
+		od.order_id;
+--- 5. Liệt kê 7 người dùng có số lượng đơn hàng nhiều nhất, thông tin hiển thị gồm: mã user, tên user, số lượng đơn hàng
+	SELECT 
+    u.user_id,
+    u.user_name,
+    COUNT(o.order_id) AS so_luong_don_hang
+	FROM 
+		users u
+	INNER JOIN 
+		orders o ON u.user_id = o.user_id
+	GROUP BY 
+		u.user_id, u.user_name
+	ORDER BY 
+		COUNT(o.order_id) DESC
+	LIMIT 7;
+--- 6. Liệt kê 7 người dùng mua sản phẩm có tên: Samsung hoặc Apple trong tên sản phẩm, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tên sản phẩm
+	SELECT 
+		u.user_id,
+		u.user_name
+	FROM 
+		users u
+	INNER JOIN 
+		orders o ON u.user_id = o.user_id
+	INNER JOIN 
+		order_details od ON o.order_id = od.order_id
+	INNER JOIN 
+		products p ON od.product_id = p.product_id
+	WHERE 
+		p.product_name LIKE '%Samsung%' OR p.product_name LIKE '%Apple%'
+	LIMIT 
+		7;
+--- 7. Liệt kê danh sách mua hàng của user bao gồm giá tiền của mỗi đơn hàng, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tổng tiền
+	SELECT 
+    u.user_id,
+    u.user_name,
+    o.order_id,
+    SUM(p.product_price) AS tong_tien
+	FROM 
+		users u
+	INNER JOIN 
+		orders o ON u.user_id = o.user_id
+	INNER JOIN 
+		order_details od ON o.order_id = od.order_id
+	INNER JOIN 
+		products p ON od.product_id = p.product_id
+	GROUP BY 
+		u.user_id, u.user_name, o.order_id;
+--- 8. Liệt kê danh sách mua hàng của user bao gồm giá tiền của mỗi đơn hàng, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tổng tiền. Mỗi user chỉ chọn ra 1 đơn hàng có giá tiền lớn nhất.
+	SELECT 
+		u.user_id,
+		u.user_name,
+		od.order_id,
+		MAX(p.product_price) AS tong_tien
+	FROM 
+		users u
+	INNER JOIN 
+		orders o ON u.user_id = o.user_id
+	INNER JOIN 
+		order_details od ON o.order_id = od.order_id
+	INNER JOIN 
+		products p ON od.product_id = p.product_id
+	GROUP BY 
+		u.user_id, u.user_name, od.order_id;
+--- 9. Liệt kê danh sách mua hàng của user bao gồm giá tiền của mỗi đơn hàng, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tổng tiền, số sản phẩm. Mỗi user chỉ chọn ra 1 đơn hàng có giá tiền nhỏ nhất.
+	SELECT 
+    u.user_id,
+    u.user_name,
+    od.order_id,
+    SUM(p.product_price) AS tong_tien,
+    COUNT(od.product_id) AS so_san_pham
+	FROM 
+		users u
+	INNER JOIN 
+		orders o ON u.user_id = o.user_id
+	INNER JOIN 
+		order_details od ON o.order_id = od.order_id
+	INNER JOIN 
+		products p ON od.product_id = p.product_id
+	GROUP BY 
+		u.user_id, u.user_name, od.order_id
+	HAVING 
+		SUM(p.product_price) = (
+			SELECT 
+				MIN(total_price)
+			FROM (
+				SELECT 
+					u.user_id,
+					od.order_id,
+					SUM(p.product_price) AS total_price
+				FROM 
+					users u
+				INNER JOIN 
+					orders o ON u.user_id = o.user_id
+				INNER JOIN 
+					order_details od ON o.order_id = od.order_id
+				INNER JOIN 
+					products p ON od.product_id = p.product_id
+				GROUP BY 
+					u.user_id, od.order_id
+			) AS min_prices
+			WHERE 
+				min_prices.user_id = u.user_id
+		);
+--- 10. Liệt kê danh sách mua hàng của user bao gồm giá tiền của mỗi đơn hàng, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tổng tiền, số sản phẩm. Mỗi user chỉ chọn ra 1 đơn hàng có số sản phẩm là nhiều nhất.
+	SELECT 
+		u.user_id,
+		u.user_name,
+		od.order_id,
+		SUM(p.product_price) AS tong_tien,
+		COUNT(od.product_id) AS so_san_pham
+	FROM 
+		users u
+	INNER JOIN 
+		orders o ON u.user_id = o.user_id
+	INNER JOIN 
+		order_details od ON o.order_id = od.order_id
+	INNER JOIN 
+		products p ON od.product_id = p.product_id
+	GROUP BY 
+		u.user_id, u.user_name, od.order_id
+	HAVING 
+		COUNT(od.product_id) = (
+			SELECT 
+				MAX(product_count)
+			FROM (
+				SELECT 
+					u.user_id,
+					od.order_id,
+					COUNT(od.product_id) AS product_count
+				FROM 
+					users u
+				INNER JOIN 
+					orders o ON u.user_id = o.user_id
+				INNER JOIN 
+					order_details od ON o.order_id = od.order_id
+				GROUP BY 
+					u.user_id, od.order_id
+			) AS max_products
+			WHERE 
+				max_products.user_id = u.user_id
+		);
